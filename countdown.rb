98b4d2de8fe24069169395f8b42bf60f5e6a6217
Tyrone <tyrone.cloma0219@gmail.com>
2022-08-09 15:21:42 -0400

    solution

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown number
+  while number != 0 do
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
+
+countdown 10
